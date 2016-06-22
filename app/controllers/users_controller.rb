75c50b1c0a2273efacb645c48dfc72c43265e913
daidea <daidea.net@gmail.com>
2016-06-22 09:19:52 +0000

    twitter follower-following first

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,6 +35,18 @@ class UsersController < ApplicationController
     end
   end
   
+  def following
+      @user  = User.find(params[:id])
+      @users = @user.following_users
+      render 'show_follow'
+  end
+
+  def followers
+    @user  = User.find(params[:id])
+    @users = @user.follower_users
+    render 'show_follower'
+  end
+  
   private
 
   def user_params
@@ -51,6 +63,7 @@ class UsersController < ApplicationController
     redirect_to root_path
     end
   end
+  
 
   
 end
